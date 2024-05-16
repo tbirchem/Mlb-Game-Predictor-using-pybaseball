943e6fd1ac107a4e456889e28fa6307b5cffbe42
tbirchem <taylor.birhem@icloud.com>
2024-05-15 21:24:56 -0700

    Hitting Data

--- Data/hittingData.py
+++ Data/hittingData.py
@@ -15,7 +15,7 @@ team_batting_stats = batting.groupby("Team").mean().reset_index()
 team_batting_stats['xOP'] = (team_batting_stats['xBA'] + team_batting_stats['xSLG'] + team_batting_stats['BB%'] + team_batting_stats['AVG'] + team_batting_stats['OPS'])
 
 xOP_baseline = team_batting_stats['xOP'].mean() - 1
-team_batting_stats['xOP'] = team_batting_stats['xOP'] - xOP_baseline
+team_batting_stats['xOP'] = team_batting_stats['xOP'] - xOP_baseline 
 
 #Sort Data
 team_batting_stats = team_batting_stats.sort_values(by="xOP", ascending=False)
